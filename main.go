96da86ccf9270a2c4bbece926cf1bc91d0f7db3f
kaleb110 <kalisha123k@gmail.com>
2026-06-05 22:50:20 +0300

    chore: prepare release

--- main.go
+++ main.go
@@ -13,7 +13,7 @@ import (
 )
 
 // AppVersion represents the current build release version
-const AppVersion string = "1.0.2"
+const AppVersion string = "1.0.3"
 
 // CurrentSchemaVersion keeps track of the JSON storage format style
 const CurrentSchemaVersion int = 1
